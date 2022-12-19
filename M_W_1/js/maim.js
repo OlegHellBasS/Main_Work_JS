a678b10ad0d232fb6834fc3fee52828b1e240bc4
OlegHellBasS <oleghellbass1@gmail.com>
2022-12-19 17:51:55 +0200

    Initial commit

---
+++ M_W_1/js/maim.js
@@ -0,0 +1,39 @@
+// - Створити змінні. Присвоїти кожному з них значення:
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+let x1 = 'helo';
+let x2 = 'owu';
+let x3 = 'com';
+let x4 = 'ua';
+let x5 = 1;
+let x6 = 10;
+let x7 = -999;
+let x8 = 123;
+let x9 = 3.14;
+let x10 = 2.7;
+let x11 = 16;
+let x12 = true;
+let x13 = false;
+console.log(x1, x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,)
+//     Вивести кожну змінну за допомогою: console.log
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+// З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = 'Oleg';
+let middleName = 'Podusovskiy';
+let lastName = 'Muhaylovich';
+console.log(firstName,middleName,lastName)
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+     let a = 100;
+     let b = '100';
+     let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+let name = prompt('Ваше імя?')
+let lastName2 = prompt('по батькови?')
+let age = +prompt('скільки вам років?')
+alert('привіт')
+console.log(name,lastName2,age)
